3ef9ea18d7586646540e780b85fee936ffe57884
irah rosete <irah.rosete@outlook.com>
2021-09-12 04:13:16 +1000

    render exercises

--- client/src/components/Exercises.jsx
+++ client/src/components/Exercises.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import ENV_URL from '../config'
+
 import Footer from './Footer'
 
 const Exercises = () => {
+  const [exercises, setExercises] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`${ENV_URL}/api/exercises/`)
+      .then((res) => {
+        setExercises(res.data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div>
-      <p>Exercises</p>
+      {exercises.map((exercise) => {
+        return (
+          <>
+            {' '}
+            <p>{exercise.date}</p>
+            <p>{exercise.activity}</p>
+            <p>{exercise.rep}</p>
+          </>
+        )
+      })}
       <Footer />
     </div>
   )
