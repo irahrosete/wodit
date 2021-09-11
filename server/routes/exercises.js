27f284559f58c0f4e05abfb193d5494122165f2d
irah rosete <irah.rosete@outlook.com>
2021-09-11 21:08:42 +1000

    add exercise routes

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -3,12 +3,21 @@ import Exercise from '../models/exercise.js'
 
 const exercisesRouter = express.Router()
 
+// get all
 exercisesRouter.route('/').get((req, res) => {
   Exercise.find()
     .then((exercises) => res.json(exercises))
     .catch((err) => res.status(400).json('Error ' + err))
 })
 
+// get one
+exercisesRouter.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercises) => res.json(exercises))
+    .catch((err) => res.status(400).json('Error ' + err))
+})
+
+// add
 exercisesRouter.route('/add').post((req, res) => {
   const firstName = req.body.firstName
   const activity = req.body.activity
@@ -20,6 +29,23 @@ exercisesRouter.route('/add').post((req, res) => {
   newExercise
     .save()
     .then(() => res.json('Exercise added!'))
+    .catch((err) => res.status(400).json('Error ' + err))
+})
+
+// update
+exercisesRouter.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => {
+      exercise.firstName = req.body.firstName
+      exercise.activity = req.body.activity
+      exercise.rep = Number(req.body.rep)
+      exercise.date = Date.parse(req.body.date)
+
+      exercise
+        .save()
+        .then(() => res.json('Exercise updated!'))
+        .catch((err) => res.status(400).json('Error ' + err))
+    })
     .catch((err) => res.status(400).json('Error ' + err))
 })
 
