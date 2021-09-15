7c6f8619023acca8b3d76e5441d26ca017b4b9f4
irah rosete <irah.rosete@outlook.com>
2021-09-15 16:17:09 +1000

    update sign up and log in pages

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LogIn = () => {
-  return <div className='mb-24 pt-16'>Log In</div>
-}
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  })
 
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setUser({ ...user, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { email, password } = user
+    if (email && password) {
+      console.log(user)
+      setUser({ email: '', password: '' })
+    }
+  }
+
+  return (
+    <div className='mb-24 pt-16'>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+
+        <label htmlFor='email'>Email</label>
+        <input type='text' name='email' required onChange={handleChange} />
+        <div className='email error'></div>
+
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          name='password'
+          required
+          onChange={handleChange}
+        />
+        <div className='password error'></div>
+
+        <button>Log in</button>
+      </form>
+    </div>
+  )
+}
 export default LogIn
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
-  return <div className='mb-24 pt-16'>Sign Up</div>
-}
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+  })
 
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setUser({ ...user, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { username, email, password } = user
+    if (username && email && password) {
+      console.log(user)
+      setUser({ username: '', email: '', password: '' })
+    }
+  }
+
+  return (
+    <div className='mb-24 pt-16'>
+      <form onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <label htmlFor='username'>Username</label>
+        <input type='text' name='username' required onChange={handleChange} />
+        <div className='username error'></div>
+
+        <label htmlFor='email'>Email</label>
+        <input type='text' name='email' required onChange={handleChange} />
+        <div className='email error'></div>
+
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          name='password'
+          required
+          onChange={handleChange}
+        />
+        <div className='password error'></div>
+
+        <button>Sign up</button>
+      </form>
+    </div>
+  )
+}
 export default SignUp
