854d152210c7b9f86d07766eb2d99ab7a37be187
irah rosete <irah.rosete@outlook.com>
2021-10-04 19:39:02 +1100

    fix jwt payload for signup and login functions

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,29 +4,92 @@ import { createToken, maxAge } from '../utils/create-token.js'
 import jwt from 'jsonwebtoken'
 
 // create new user
+// const signUpUser = async (req, res) => {
+//   const { username, email, password } = req.body
+
+//   try {
+//     const user = await User.create({ username, email, password })
+//     const token = createToken({ userid: user._id, username: user.username })
+//     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+//     res.status(201).json({ userid: user._id, username: user.username })
+//   } catch (err) {
+//     const errors = handleErrors(err)
+//     res.status(400).json({ errors })
+//   }
+// }
+
+// const signUpUser = (req, res) => {
+//   const newUser = new User(req.body)
+//   newUser.save((err, user) => {
+//     if (err) {
+//       const errors = handleErrors(err)
+//       res.status(400).json({ errors })
+//     }
+//     const token = createToken({ userid: user._id, username: user.username })
+//     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 }) // cookie expects time in millisecond
+//     res.status(201).json({ userid: user._id, username: user.username })
+//   })
+// }
+
 const signUpUser = (req, res) => {
-  const newUser = new User(req.body)
-  newUser.save((err, user) => {
-    if (err) {
+  const { username, email, password } = req.body
+  const user = User.create({ username, email, password })
+
+  user
+    .then((response) => {
+      const token = createToken({
+        userid: response._id,
+        username: response.username,
+      })
+      res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 }) // cookie expects time in millisecond
+      res.status(201).json({
+        id: response.id,
+        username: response.username,
+        email: response.email,
+      })
+      console.log({
+        id: response.id,
+        username: response.username,
+        email: response.email,
+      })
+    })
+    .catch((err) => {
       const errors = handleErrors(err)
       res.status(400).json({ errors })
-    }
-    const token = createToken({ userid: user._id, username: user.username })
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 }) // cookie expects time in millisecond
-    res.status(201).json({ userid: user._id, username: user.username })
-  })
+      console.log(errors)
+    })
 }
 
 // authenticate existing user
+// const logInUser = (req, res) => {
+//   const { email, password } = req.body
+
+//   User.findOne({ email }, (err, user) => {
+//     if (err) {
+//       const errors = handleErrors(err)
+//       res.status(400).json({ errors })
+//     }
+//     if (!user || !user.comparePassword(password)) {
+//       res.status(400).json({ message: 'Authentication failed' })
+//     }
+//     const token = createToken({ userid: user._id, username: user.username })
+//     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 }) // cookie expects time in millisecond
+//     res.status(200).json({ userid: user._id, username: user.username })
+//   })
+// }
+
 const logInUser = (req, res) => {
   const { email, password } = req.body
 
   const user = User.login(email, password)
-  const token = createToken({ userid: user._id, username: user.username })
-  res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 }) // cookie expects time in millisecond, jwt expects time in seconds
 
   user
     .then((response) => {
+      const token = createToken({
+        userid: response._id,
+        username: response.username,
+      })
+      res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 }) // cookie expects time in millisecond, jwt expects time in seconds
       res.status(200).json({
         id: response.id,
         username: response.username,
