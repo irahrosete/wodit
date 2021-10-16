ab7427c2eb7ac2d8f2e43962d6dedc932b5983df
irah rosete <irah.rosete@outlook.com>
2021-10-16 17:30:03 +1100

    style accordion in Girls component

--- client/src/pages/Girls.jsx
+++ client/src/pages/Girls.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { data } from '../Data'
 import { FiPlus, FiMinus } from 'react-icons/fi'
-import { IconContext } from 'react-icons'
 
 const Girls = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -13,9 +12,9 @@ const Girls = () => {
   return (
     <div className='flex items-center justify-center flex-col pt-24'>
       <div className='flex flex-col items-center'>
-        <h2 className='textWod text-2xl mt-3 uppercase'>The Girls</h2>
+        <h2 className='textWod text-2xl my-3 uppercase '>The Girls</h2>
       </div>
-      {data.map((item, id) => {
+      {data.map((item) => {
         return (
           <div
             className='flex items-center justify-center flex-col'
@@ -24,16 +23,31 @@ const Girls = () => {
             }}
             key={item.id}
           >
-            <div className='flex justify-between items-center'>
-              <h2>{item.name}</h2>
-              <span>{isOpen === item.id ? <FiMinus /> : <FiPlus />}</span>
-            </div>
-            <h3>{item.sub}</h3>
-            <p>{item.desc1}</p>
-            <p>{item.desc2}</p>
-            <p>{item.desc3}</p>
-            <p>{item.desc4}</p>
-            <p>{item.desc5}</p>
+            {isOpen === item.id ? (
+              <div className='flex justify-between items-center boxWod bg-wodYellow my-1'>
+                <h2 className='textWod text-xl px-4'>{item.name}</h2>
+                <span className='px-3'>
+                  <FiMinus />
+                </span>
+              </div>
+            ) : (
+              <div className='flex justify-between items-center boxWod bg-wodGray my-1'>
+                <h2 className='textWod text-xl px-4'>{item.name}</h2>
+                <span className='px-3'>
+                  <FiPlus />
+                </span>
+              </div>
+            )}
+            {isOpen === item.id ? (
+              <div className='boxWod textWod px-8'>
+                <h3 className='uppercase pb-2'>{item.sub}</h3>
+                <p>{item.desc1}</p>
+                <p>{item.desc2}</p>
+                <p>{item.desc3}</p>
+                <p>{item.desc4}</p>
+                <p>{item.desc5}</p>
+              </div>
+            ) : null}
           </div>
         )
       })}
