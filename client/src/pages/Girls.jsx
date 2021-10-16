b6fdfb168e59a3d2c5700434eb1e728061af4599
irah rosete <irah.rosete@outlook.com>
2021-10-16 20:01:01 +1100

    fetch girls data from api

--- client/src/pages/Girls.jsx
+++ client/src/pages/Girls.jsx
@@ -1,40 +1,42 @@
 import React, { useState, useEffect } from 'react'
-import { data } from '../Data'
+import axios from 'axios'
+
 import { FiPlus, FiMinus } from 'react-icons/fi'
+
+import ENV_URL from '../config'
 
 const Girls = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const [girls, setGirls] = useState([])
 
   const toggle = (id) => {
     isOpen === id ? setIsOpen(null) : setIsOpen(id)
   }
 
-  // useEffect(() => {
-  //      axios
-  //         .get(
-  //           `${ENV_URL}/api/exercises/get?userid=${user.userid}&username=${user.username}`
-  //         )
-  //         .then((res) => {
-  //           setExercises(res.data)
-  //         })
-  //         .catch((err) => console.log(err))
-  // }, [])
+  useEffect(() => {
+    axios
+      .get(`${ENV_URL}/api/girls/`)
+      .then((res) => {
+        setGirls(res.data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
 
   return (
     <div className='flex items-center justify-center flex-col pt-24'>
       <div className='flex flex-col items-center'>
         <h2 className='textWod text-2xl my-3 uppercase '>The Girls</h2>
       </div>
-      {data.map((item) => {
+      {girls.map((item) => {
         return (
           <div
             className='flex items-center justify-center flex-col'
             onClick={() => {
-              toggle(item.id)
+              toggle(item._id)
             }}
             key={item.id}
           >
-            {isOpen === item.id ? (
+            {isOpen === item._id ? (
               <div className='flex justify-between items-center boxWod bg-wodYellow my-1'>
                 <h2 className='textWod text-lg px-4'>{item.name}</h2>
                 <span className='px-3'>
@@ -49,16 +51,16 @@ const Girls = () => {
                 </span>
               </div>
             )}
-            {isOpen === item.id ? (
-              <div className='boxWod textWod px-8'>
-                <h3 className='uppercase text-sm pb-3'>{item.sub}</h3>
-                <p>{item.desc1}</p>
-                <p>{item.desc2}</p>
-                <p>{item.desc3}</p>
-                <p>{item.desc4}</p>
-                <p>{item.desc5}</p>
+            {isOpen === item._id ? (
+              <div className='boxWod textWod'>
+                <h3 className='uppercase text-sm pb-3 px-8'>{item.sub}</h3>
+                <p className='px-14'>{item.desc1}</p>
+                <p className='px-14'>{item.desc2}</p>
+                <p className='px-14'>{item.desc3}</p>
+                <p className='px-14'>{item.desc4}</p>
+                <p className='px-14'>{item.desc5}</p>
                 {item.target ? (
-                  <p className='uppercase text-sm pt-3 text-right'>
+                  <p className='uppercase text-sm pt-3 px-8 text-right'>
                     target: {item.target}
                   </p>
                 ) : null}
--- server/controllers/girls.js
+++ server/controllers/girls.js
@@ -1,18 +1,10 @@
 import Girl from '../models/girl.js'
 import { data } from '../utils/seedGirls.js'
 
-// categoryRouter.get(
-//   '/seed',
-//   expressAsyncHandler(async (req, res) => {
-//     const createdCategories = await Category.insertMany(seed.categories)
-//     res.send({ createdCategories })
-//   })
-// )
-
 // seed girls db
 const seed = (req, res) => {
   const seededGirls = Girl.insertMany(data)
-    .then(() => res.send({ seededGirls }))
+    .then(() => res.send(seededGirls))
     .catch((err) => res.status(400).json('Error ' + err))
 }
 
