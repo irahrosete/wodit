f3a0eff1030e194d3e54c3173cb9f4facc469370
irah rosete <irah.rosete@outlook.com>
2021-12-13 00:26:55 +1100

    update wodeditwarmup to take required wod fields

--- client/src/components/WodEditWarmUp.jsx
+++ client/src/components/WodEditWarmUp.jsx
@@ -34,7 +34,15 @@ const WodEditWarmUp = () => {
           .substring(0, 10)}&userid=${user.userid}&username=${user.username}`
       )
       .then((res) => {
-        setWod(res.data[0])
+        res.data[0]
+          ? setWod(res.data[0])
+          : setWod({
+              userid: user.userid,
+              username: user.username,
+              warmup: '',
+              workout: '',
+              date: wdate.toISOString().substring(0, 10),
+            })
       })
       .catch((err) => console.log(err))
   }, [wdate, user.userid, user.username])
